0c7c7e47124abecfb5f87beefde343f3a6ecdc7d
Fernando Alencar <fernando@fernandoalencar.com.br>
2023-07-29 08:58:38 -0400

    .

--- dist/app.handler.d.ts
+++
@@ -1,5 +0,0 @@
-import { Message } from '@aws-sdk/client-sqs';
-export declare class AppHandler {
-    handleMessage(message: Message[]): Promise<void>;
-    onProcessingError(error: Error, message: Message): void;
-}
--- dist/app.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { SqsService } from '@ssut/nestjs-sqs';
-export declare class AppService {
-    private readonly sqsService;
-    constructor(sqsService: SqsService);
-    execute(): Promise<void>;
-}
